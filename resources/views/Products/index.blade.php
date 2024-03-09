08b33354541e9eb76d18adc52ee93db07eeb4d13
MaxiKapko <maxi.kapko73@gmail.com>
2024-03-08 23:07:02 -0300

    Created icon-Products index-cart index

---
+++ resources/views/Products/index.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.app')
+
+@section('content')
+<!-- Content for the main section -->
+<header class="bg-white dark:bg-gray-800 shadow">
+    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            Products
+        </h2>
+    </div>
+</header>
+@endsection
